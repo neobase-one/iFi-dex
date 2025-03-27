aa162f1ff9292e101bcd9bc4706dc71cafbf70f6
Christian Borst <christianoborst@gmail.com>
2025-03-27 12:41:50 -0400

    Add gratuitous comments to DEX deploy script

--- misc/scripts/dex-deployer.ts
+++ misc/scripts/dex-deployer.ts
@@ -30,6 +30,11 @@ const args = commandLineArgs([
   { name: "contract-json", type: String },
 ]);
 
+// This is a static address which is the EVM address for the module with the name "nativedex"
+// It is obtained by taking the first 20 bytes of the keccak256 hash of the string "nativedex", then
+// converting that to an EVM address by parsing the address' bytes as an EIP-55 address.
+// A convenient way to get this is by using `althea q auth module-account` and using that address with
+// `althea debug addr` to get an EVM address.
 const nativedexModuleAddress = "0xe3ADB86F7F0425d08ebD0dfFEbd2eEf19E12D30e";
 
 // sets the gas price for all contract deployments
@@ -37,6 +42,7 @@ const overrides = {
   //gasPrice: 100000000000
 };
 
+// This object holds all the paths to the contract artifacts, which can change depending on the environment.
 type CrocPaths = {
   dex: string;
   hot: string;
@@ -53,6 +59,7 @@ type CrocPaths = {
   upgrade_test: string;
 };
 
+// Collects all the paths to the contract artifacts, which can change depending on the environment.
 function get_paths(root: string, include_sol: boolean): CrocPaths {
   if (include_sol) {
     return {
@@ -88,12 +95,19 @@ function get_paths(root: string, include_sol: boolean): CrocPaths {
   };
 }
 
+// Actually performs the deploy of all the contracts, then ties them together and configures the DEX for the first pool's creation
+// The DEX is a collection of contracts, which are CrocSwapDex (the main contract) and several "callpaths" which are used to get around
+// the maximum contract size limit of the EVM. The callpaths are installed via the "BootProxy" callpath (installed in the DEX's constructor).
+// This function first deploys all the contracts, then installs them, then sets some required DEX parameters.
+// The console log statements are important, they will be detected by the Rust tests and used as the source of contract addresses, so do not change their
+// format without careful consideration. See the bootstrapping.rs file for more info.
 async function deploy() {
   var startTime = new Date();
   const provider = await new ethers.providers.JsonRpcProvider(args["eth-node"]);
   let wallet = new ethers.Wallet(args["eth-privkey"], provider);
   let artifacts_root = args["artifacts-root"];
 
+  // Attempt to contact the Ethereum node before getting started (timeout after 10 minutes)
   var success = false;
   while (!success) {
     var present = new Date();
@@ -127,6 +141,7 @@ async function deploy() {
   var bytecode;
   var factory;
 
+  // Deploy the CrocSwapDex contract
   ({ abi, bytecode } = getContractArtifacts(contract_paths.dex));
   factory = new ethers.ContractFactory(abi, bytecode, wallet);
   const dex = (await factory.deploy(overrides)) as CrocSwapDex;
@@ -134,6 +149,7 @@ async function deploy() {
   const dexAddress = dex.address;
   console.log("CrocSwapDex deployed at Address - ", dexAddress);
 
+  // Deploy the HotProxy contract (not installed yet)
   ({ abi, bytecode } = getContractArtifacts(contract_paths.hot));
   factory = new ethers.ContractFactory(abi, bytecode, wallet);
   const hot = (await factory.deploy(overrides)) as HotProxy;
@@ -141,6 +157,7 @@ async function deploy() {
   const hotAddress = hot.address;
   console.log("HotProxy deployed at Address - ", hotAddress);
 
+  // Deploy the ColdPath contract (not installed yet)
   ({ abi, bytecode } = getContractArtifacts(contract_paths.cold));
   factory = new ethers.ContractFactory(abi, bytecode, wallet);
   const cold = (await factory.deploy(overrides)) as ColdPath;
@@ -148,6 +165,7 @@ async function deploy() {
   const coldAddress = cold.address;
   console.log("ColdPath deployed at Address - ", coldAddress);
 
+  // Deploy the WarmPath contract (not installed yet)
   ({ abi, bytecode } = getContractArtifacts(contract_paths.warm));
   factory = new ethers.ContractFactory(abi, bytecode, wallet);
   const warm = (await factory.deploy(overrides)) as WarmPath;
@@ -155,6 +173,7 @@ async function deploy() {
   const warmAddress = warm.address;
   console.log("WarmPath deployed at Address - ", warmAddress);
 
+  // Deploy the LongPath contract (not installed yet)
   ({ abi, bytecode } = getContractArtifacts(contract_paths.long));
   factory = new ethers.ContractFactory(abi, bytecode, wallet);
   const long = (await factory.deploy(overrides)) as LongPath;
@@ -162,6 +181,7 @@ async function deploy() {
   const longAddress = long.address;
   console.log("LongPath deployed at Address - ", longAddress);
 
+  // Deploy the MicroPaths contract (not installed yet)
   ({ abi, bytecode } = getContractArtifacts(contract_paths.micro));
   factory = new ethers.ContractFactory(abi, bytecode, wallet);
   const micro = (await factory.deploy(overrides)) as MicroPaths;
@@ -169,6 +189,7 @@ async function deploy() {
   const microAddress = micro.address;
   console.log("MicroPaths deployed at Address - ", microAddress);
 
+  // Deploy the KnockoutFlagPath contract (not installed yet)
   ({ abi, bytecode } = getContractArtifacts(contract_paths.knockout_flag));
   factory = new ethers.ContractFactory(abi, bytecode, wallet);
   const knockout_flag = (await factory.deploy(overrides)) as KnockoutFlagPath;
@@ -179,6 +200,7 @@ async function deploy() {
     knockout_flagAddress
   );
 
+  // Deploy the KnockoutLiqPath contract (not installed yet)
   ({ abi, bytecode } = getContractArtifacts(contract_paths.knockout_liq));
   factory = new ethers.ContractFactory(abi, bytecode, wallet);
   const knockout_liq = (await factory.deploy(overrides)) as KnockoutLiqPath;
@@ -186,6 +208,7 @@ async function deploy() {
   const knockout_liqAddress = knockout_liq.address;
   console.log("KnockoutLiqPath deployed at Address - ", knockout_liqAddress);
 
+  // Deploy the SafeModePath contract (not installed yet)
   ({ abi, bytecode } = getContractArtifacts(contract_paths.safe_mode));
   factory = new ethers.ContractFactory(abi, bytecode, wallet);
   const safe_mode = (await factory.deploy(overrides)) as SafeModePath;
@@ -193,6 +216,7 @@ async function deploy() {
   const safe_modeAddress = safe_mode.address;
   console.log("SafeModePath deployed at Address - ", safe_modeAddress);
 
+  // Deploy the governance contract "CrocPolicy", which does not yet control the DEX
   ({ abi, bytecode } = getContractArtifacts(contract_paths.policy));
   factory = new ethers.ContractFactory(abi, bytecode, wallet);
   const policy = (await factory.deploy(
@@ -204,14 +228,15 @@ async function deploy() {
   const policyAddress = policy.address;
   console.log("CrocPolicy deployed at Address - ", policyAddress);
 
+  // Deploy the CrocQuery periphery contract, which is not directly connected to the DEX
   ({ abi, bytecode } = getContractArtifacts(contract_paths.query));
-
   factory = new ethers.ContractFactory(abi, bytecode, wallet);
   const query = (await factory.deploy(dexAddress, overrides)) as CrocQuery;
   await query.deployed();
   const queryAddress = query.address;
   console.log("CrocQuery deployed at Address - ", queryAddress);
 
+  // Deploy the CrocImpact periphery contract, which is not directly connected to the DEX
   ({ abi, bytecode } = getContractArtifacts(contract_paths.impact));
   factory = new ethers.ContractFactory(abi, bytecode, wallet);
   const impact = (await factory.deploy(dexAddress, overrides)) as CrocImpact;
@@ -219,6 +244,8 @@ async function deploy() {
   const impactAddress = impact.address;
   console.log("CrocImpact deployed at Address - ", impactAddress);
 
+  // Deploy a test upgrade contract, this is a copy of the ColdPath contract which requires that
+  // it will be installed on callpath 33. See the DEX_UPGRADE test for more.
   ({ abi, bytecode } = getContractArtifacts(contract_paths.upgrade_test));
   factory = new ethers.ContractFactory(abi, bytecode, wallet);
   const upgrade = (await factory.deploy(overrides)) as ColdPathUpgrade;
@@ -226,12 +253,16 @@ async function deploy() {
   const upgradeAddress = upgrade.address;
   console.log("ColdPathUpgrade deployed at Address - ", upgradeAddress);
 
+
+  // Now that all the contracts have been deployed, they must be connected to the main CrocSwapDex contract
+  // by calling the BootPath contract's protocolCmd function.
   console.log("Installing CrocSwap contracts");
 
   let abiCoder = new ethers.utils.AbiCoder();
   let cmd;
   let tx;
 
+  // These are hardcoded values pulled from the solidity contracts, see mixins/StorageLayout.sol for the source values
   const BOOT_PROXY_IDX = 0;
   const SWAP_PROXY_IDX = 1;
   const LP_PROXY_IDX = 2;
@@ -243,7 +274,7 @@ async function deploy() {
   const SAFE_MODE_PROXY_PATH = 9999;
 
   // use protocolCmd to install paths
-  // install coldpath
+  // Install ColdPath in callpath 3
   cmd = abiCoder.encode(
     ["uint8", "address", "uint16"],
     [21, coldAddress, COLD_PROXY_IDX]
@@ -251,7 +282,7 @@ async function deploy() {
   tx = await dex.protocolCmd(BOOT_PROXY_IDX, cmd, true);
   await tx.wait();
 
-  // // install longpath
+  // Install LongPath in callpath 4
   console.log("Installing LongPath");
   cmd = abiCoder.encode(
     ["uint8", "address", "uint16"],
@@ -260,7 +291,7 @@ async function deploy() {
   tx = await dex.protocolCmd(BOOT_PROXY_IDX, cmd, true);
   await tx.wait();
 
-  // // install warm path
+  // Install WarmPath in callpath 2
   console.log("Installing WarmPath");
   cmd = abiCoder.encode(
     ["uint8", "address", "uint16"],
@@ -269,7 +300,7 @@ async function deploy() {
   tx = await dex.protocolCmd(BOOT_PROXY_IDX, cmd, true);
   await tx.wait();
 
-  // // install hot proxy path
+  // Install HotProxy in callpath 1
   console.log("Installing HotPath Proxy");
   cmd = abiCoder.encode(
     ["uint8", "address", "uint16"],
@@ -278,7 +309,7 @@ async function deploy() {
   tx = await dex.protocolCmd(BOOT_PROXY_IDX, cmd, true);
   await tx.wait();
 
-  // // install micro paths
+  // Install MicroPaths in callpath 5
   console.log("Installing MicroPaths");
   cmd = abiCoder.encode(
     ["uint8", "address", "uint16"],
@@ -287,7 +318,7 @@ async function deploy() {
   tx = await dex.protocolCmd(BOOT_PROXY_IDX, cmd, true);
   await tx.wait();
 
-  // // install knockout lp proxy path
+  // Install KnockoutLiqPath in callpath 7
   console.log("Installing KnockoutLiqPath");
   cmd = abiCoder.encode(
     ["uint8", "address", "uint16"],
@@ -296,7 +327,7 @@ async function deploy() {
   tx = await dex.protocolCmd(BOOT_PROXY_IDX, cmd, true);
   await tx.wait();
 
-  // // install cross knockout cross proxy path
+  // Install KnockoutFlagPath in callpath 3500
   console.log("Installing KnockoutFlagPath");
   cmd = abiCoder.encode(
     ["uint8", "address", "uint16"],
@@ -305,7 +336,7 @@ async function deploy() {
   tx = await dex.protocolCmd(BOOT_PROXY_IDX, cmd, true);
   await tx.wait();
 
-  // // install safe mode path
+  // Install SafeModePath in callpath 9999
   console.log("Installing SafeModePath");
   cmd = abiCoder.encode(
     ["uint8", "address", "uint16"],
@@ -316,10 +347,37 @@ async function deploy() {
 
   // Note we do not install the ColdPathUpgrade since it's use is at test runtime
 
+  // Next we must set some required initial parameters for the DEX.
+  // The initial pool liquidity is an amount of tokens which must be provided on every pool creation,
+  // and that liquidity must forever be locked into the DEX. This provides a minimum amount of liquidity to make sure that
+  // the math all works out in the future even if a pool is "empty".
   console.log("Setting initial pool liquidity");
   let setPoolLiqCmd = abiCoder.encode(["uint8", "uint128"], [112, 1]);
   tx = await dex.protocolCmd(3, setPoolLiqCmd, true);
   await tx.wait();
+
+  // Next we set up two "pool templates", which restrict new pools to adhere to a common format.
+  // Regular users are allowed to create new pools with any token pair they want, but they are limited to using only
+  // the configured pool templates to control the pool parameters. DEX Governance is allowed to change the templates,
+  // change individual pools, and add new templates - but regular users are not able to do so.
+  // We set up one pool template for stable-stable token pairs and another for volatile-volatile token pairs.
+
+  /* A pool template consists of:
+      PoolIdx: A unique ID which will forever identify pools created using this template
+      FeeRate: The fee rate charged on trades, in basis points (1/100th of a percent) - fees are split between LPers and the protocol
+      TickSize: The minimum price movement allowed in the pool, in log_2(ticks) - a tick is 1/10000th of a percent and this value
+                is actually the exponent so 3 would be 2^3 = 8 ticks. A lower tick size will cause more gas consumption when swapping
+                across large price movements, so it's a tradeoff between precision and gas cost.
+      JITTime: The amount of time in seconds that a LPer must wait between minting a concentrated position and burning it. This is a 
+               means to discourage concentrated position sandwich attacks where one LPer tries to earn all the fees by observing swaps,
+               manipulating blocks (possibly via collusion) and sandwiching swaps with a concentrated mint and burn. By not having this
+               value set then ambient (full range) LPers will have their rewards diluted by the sandwicher (sandwitch?) while still suffering
+               Impermanent Loss. Such a situation would be toxic and lead to poor ambient liquidity in the ecosystem.
+      KnockoutBits: Configuration for knockout liquidity, see below
+      OracleFlags: Flags for configuring an oracle contract, not used in this deployment. Currently the DEX allows either "permissioned" or
+                   "non-permissioned" pools. Permissioned pools require an oracle to be set and the oracle contract will be called to approve or deny
+                   actions on permissioned pools including new pool creation, minting, burning, and swapping. All our pools are non-permissioned.
+  */
 
   /* Setting the knockout bits is a bit complicated, from the KnockoutLiq comments:
       The fields are set in the following order from most to least significant bit:
@@ -354,6 +412,8 @@ async function deploy() {
   let stablePairBits = stablePairWidthBits | inRangePlaceBits | outOfRangePlaceBits;
   // Allow pools on the volatile pair template to have any knockout position with width of 1024 bits
   let volatilePairBits = volatilePairWidthBits | inRangePlaceBits | outOfRangePlaceBits;
+
+
   console.log("Setting default pool templates (index 36000, 36001)");
   // Set the stable pairs to use index 36000, have a fee of 0.25%, tick size of 1, 10 second jit time, stable pair bits, and no oracle
   let templateCmd = abiCoder.encode(
